9b280d1489074f015354d3b16aa558e7c95e3c00
miclemabasie <miclemabasie3@gmail.com>
2023-03-28 23:40:24 +0100

    sharing post via email

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -41,4 +43,35 @@ def post_detail(request, year, month, day, post):
     }
     template_name = "blog/post/detail.html"
 
+    return render(request, template_name, context)
+
+
+def post_share(request, post_id):
+    # Retrieve the post from the database by the id
+    post = get_object_or_404(Post, id=post_id, status="published")
+    sent = False
+
+    if request.method == "POST":
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields passed all validation
+            cd = form.cleaned_data
+            # .....Send mail
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd.get('name')} recommends you read {post.title}"
+            message = f"REad {post.title} at {post_url} \n\n {cd.get('name')}'s comments: {cd.get('comments')}"
+            send_mail(subject, message, "admin@mail.com", [cd.get("to")])
+
+            sent = True
+
+    else:
+        form = EmailPostForm()
+
+    template_name = "blog/post/share.html"
+    context = {
+        "post": post,
+        "sent": sent,
+        "form": form,
+    }
     return render(request, template_name, context)
--- my_blog/settings.py
+++ my_blog/settings.py
@@ -87,6 +87,8 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
