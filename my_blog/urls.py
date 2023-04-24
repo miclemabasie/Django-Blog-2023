6903925c883c0b799706386102822f5d7a6a4931
miclemabasie <miclemabasie3@gmail.com>
2023-04-24 17:24:03 +0100

    added tailwindcss

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
+from django.conf import settings
+from django.conf.urls.static import static
 
 sitemaps = {
     "posts": PostSitemap,
@@ -32,3 +34,7 @@ urlpatterns = [
         name="django.contrib.sitemaps.views.sitemaps",
     ),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
