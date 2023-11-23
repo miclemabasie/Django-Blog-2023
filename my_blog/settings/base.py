b62028aa455325628067ff9c1c3be00e5ac88b35
miclemabasie <miclemabasie3@gmail.com>
2023-11-23 11:58:02 +0100

    removed logging -temporalily

--- my_blog/settings/base.py
+++ my_blog/settings/base.py
@@ -142,42 +142,42 @@ MEDIA_URL = "/media/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-import logging
-import logging.config
-from django.utils.log import DEFAULT_LOGGING
+# import logging
+# import logging.config
+# from django.utils.log import DEFAULT_LOGGING
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-LOG_LEVEL = "INFO"
+# LOG_LEVEL = "INFO"
 
-logging.config.dictConfig(
-    {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "console": {
-                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
-            },
-            "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
-            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
-        },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "formatter": "console",
-            },
-            "file": {
-                "level": "INFO",
-                "class": "logging.FileHandler",
-                "formatter": "file",
-                "filename": "logs/blog.log",
-            },
-            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
-        },
-        "loggers": {
-            "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
-            "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
-            "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
-        },
-    }
-)
+# logging.config.dictConfig(
+#     {
+#         "version": 1,
+#         "disable_existing_loggers": False,
+#         "formatters": {
+#             "console": {
+#                 "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+#             },
+#             "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+#             "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+#         },
+#         "handlers": {
+#             "console": {
+#                 "class": "logging.StreamHandler",
+#                 "formatter": "console",
+#             },
+#             "file": {
+#                 "level": "INFO",
+#                 "class": "logging.FileHandler",
+#                 "formatter": "file",
+#                 "filename": "logs/blog.log",
+#             },
+#             "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+#         },
+#         "loggers": {
+#             "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
+#             "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
+#             "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+#         },
+#     }
+# )
